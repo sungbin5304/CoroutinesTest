186cab1c94b813f6a237e88af5c309320e347b90
sungbin5304 <sungbin.dev@gmail.com>
2020-10-03 16:07:17 +0900

    a

--- app/src/main/java/me/sungbin/coroutinestest/MainActivity.kt
+++ app/src/main/java/me/sungbin/coroutinestest/MainActivity.kt
@@ -3,28 +3,39 @@ package me.sungbin.coroutinestest
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.*
-import java.util.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Dispatchers.Unconfined).launch {
-            log(Date().time)
-            val deferred = async {
-                log(Date().time)
-                var i = 0
-                while (i < 5) {
-                    delay(500)
-                    println("lazy async $i")
-                    yield()
-                    i++
-                }
-                i
+        suspend fun one(): Int {
+            var i = 0
+            repeat(5) {
+                delay(1000)
+                i++
             }
-            log(deferred.await())
+            return i
+        }
+
+        suspend fun two(): Int {
+            var i = 0
+            repeat(10) {
+                delay(1000)
+                i++
+            }
+            return i
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            log("hello world!")
+        }.run {
+            start()
+            cancel()
         }
     }
 }
